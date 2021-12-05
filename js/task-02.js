a894cbe7675b1e5f308821d1dbfefaa2e10fb02b
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2021-12-06 00:59:05 +0200

    Задание__2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,29 @@
+// Задание 2
+// В HTML есть пустой список ul#ingredients.
+// В JavaScript есть массив строк.
+// Напиши скрипт, который для каждого элемента массива ingredients:
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul.ingredients.
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+/* Решение */
+const ulElIngredients = document.getElementById("ingredients");
+
+const arrLiEl = ingredients.map((el) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = el;
+  return liEl;
+});
+
+ulElIngredients.append(...arrLiEl);
